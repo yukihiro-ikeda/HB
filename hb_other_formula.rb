7b6934152efae4e9e99615ecb6c1d81b25947383
yukihiro ikeda <headlessrecord@gmail.com>
2023-10-17 03:20:08 +0900

    hb_other_formula_playcode

--- hb_other_formula.rb
+++ hb_other_formula.rb
@@ -1,14 +1,18 @@
 def style
   io = gets.chomp.chars
   com = [*"0".."9"].sample(3)
-  tmp = []
+  hit_tmp = []
+  blow_tmp = []
   for i in 0..2 do
     if io[i] == com[i]
-      tmp.push(i)
+      hit_tmp.push(i)
+    elsif com.include?(io[i])
+      blow_tmp.push(i)
     end
   end
-  puts "#{tmp.length}hit"
+  puts "#{hit_tmp.length}hit,#{blow_tmp.length}blow"
   p com
+  # binding.irb
 end
 
 style
