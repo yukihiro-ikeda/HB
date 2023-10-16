51ecddefce6391ea39b413f880bdeb5bd095b6cf
yukihiro ikeda <headlessrecord@gmail.com>
2023-10-17 03:53:37 +0900

    hb_other_formula.rb_correction

--- hb_other_formula.rb
+++ hb_other_formula.rb
@@ -1,18 +1,28 @@
 def style
-  io = gets.chomp.chars
   com = [*"0".."9"].sample(3)
-  hit_tmp = []
-  blow_tmp = []
-  for i in 0..2 do
-    if io[i] == com[i]
-      hit_tmp.push(i)
-    elsif com.include?(io[i])
-      blow_tmp.push(i)
+  while 
+    io = gets.chomp.chars
+
+    hit_tmp = []
+    blow_tmp = []
+    for i in 0..2 do
+      if io[i] == com[i]
+        hit_tmp.push(i)
+      elsif com.include?(io[i])
+        blow_tmp.push(i)
+      end
+    end
+    if hit_tmp.length == 3
+      puts "当てました"
+      puts "COM's Numberは#{com.map(&:to_i)}です"
+      # p com
+      break
+    else
+      puts "#{hit_tmp.length}hit,#{blow_tmp.length}blow"
+      # p com
+    # binding.irb
     end
   end
-  puts "#{hit_tmp.length}hit,#{blow_tmp.length}blow"
-  p com
-  # binding.irb
 end
 
 style
